3e85b28477bc59d8dcb84d2ab2ec7b8aa5eb18ce
luna18-meet <luna.abujaber@hotmail.com>
2017-01-19 18:31:40 +0200

    commit

--- webapp.py
+++ webapp.py
@@ -1,33 +1,89 @@
-from flask import Flask
+from flask import Flask, url_for, flash, redirect, request, render_template , g
 from model import *
+from flask import session as login_session
 
 app = Flask(__name__)
+app.secret_key = "MY_SUPER_SECRET_KEY"
 
 engine = create_engine('sqlite:///fizzBuzz.db')
 Base.metadata.bind = engine
 DBSession = sessionmaker(bind=engine, autoflush=False)
 session = DBSession()
 
+def verify_password(email,password):
+	customer = session.query(Customer).filter_by(email=email).first()
+	if not customer or not customer.verify_password(password):
+		return False
+	g.customer = customer
+	return True
+
 @app.route('/')
 @app.route('/inventory')
 def inventory():
-	return "To be implemented"
+	items = session.query(Product).all()
+	return render_template("inventory.html",items=items)
+
 
 @app.route('/login', methods = ['GET', 'POST'])
 def login():
-	return "To be implmented"
+	if request.method == 'GET':
+		return render_template('login.html')
+	elif request.method == 'POST':
+		email = request.form['email']
+		password = request.form['password']
+		if email=="" or password=="":
+			flash("Missing Arguments")
+			return redirect(url_for(login))
+		if verify_password(email,password):
+			customer=session.query(Customer).filter_by(email=email).one()
+			flash('Login Successful, welcome, %s' % customer.name)
+			login_session['name']=customer.name
+			login_session['email']=customer.email
+			login_session['id']=customer.id
+			return redirect(url_for('inventory'))
+		else:
+			#incorrect username/password
+			flash("Incorrect username/password combination")
+			return redirect(url_for('login'))
+
 
 @app.route('/newCustomer', methods = ['GET','POST'])
 def newCustomer():
-	return "To be implemented"
+	if request.method == 'POST':
+		name = request.form['name']
+		email = request.form['email']
+		password = request.form['password']
+		address = request.form['address']
+		if name =="" or email=="" or password=="":
+			flash("Your form is missing arguments")
+			return redirect(url_for('newCustomer'))
+		if session.query(Customer).filter_by(email = email).first() is not None:
+			flash("A user with this email address already exists")
+			return redirect(url_for('newCustomer'))
+		customer = Customer(name = name, email=email, address = address)
+		customer.hash_password(password)
+		session.add(customer)
+		shoppingCart = ShoppingCart(customer=customer)
+		session.add(shoppingCart)
+		session.commit()
+		flash("User Created Successfully!")
+		return redirect(url_for('inventory'))
+	else:
+		return render_template('newCustomer.html')
 
 @app.route("/product/<int:product_id>")
 def product(product_id):
-	return "To be implemented"
+	product = session.query(Product).filter_by(id=product_id).one()
+	return render_template('product.html' , product=product)
 
 @app.route("/product/<int:product_id>/addToCart", methods = ['POST'])
 def addToCart(product_id):
-	return "To be implemented"
+	if 'id' not in login_session:
+		flash("You must be logged in to perform this action")
+		return redirect(url_for('login'))
+	quantity = request.form['quantity']
+	product = session.query
+	#CONTINUE FROM HERE - ADD TO CART BUTTON (SECTION II)
 
 @app.route("/shoppingCart")
 def shoppingCart():
@@ -54,4 +110,4 @@ def logout():
 	return "To be implmented"
 
 if __name__ == '__main__':
-    app.run()
+    app.run(debug=True)
